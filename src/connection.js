58174bf087f81fa18e8904025c7feac1c79534d6
icaroaf <caroassisfreitas@globo.com>
2021-09-28 19:56:53 -0300

    configura banco de dados com querybuilder

--- src/connection.js
+++ src/connection.js
@@ -1,20 +1,11 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-    user: '',
-    host: '',
-    database: '',
-    password: '',
-    port: 5432,
-    ssl: {
-        rejectUnauthorized: false
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host: 'ec2-54-209-52-160.compute-1.amazonaws.com',
+        user: 'eeomqmvwtjlnrs',
+        password: '9e127672ea54c7bb8a4beeff7fd437bfeafcaa8eb418c3fb243e58fd58e92cf3',
+        database: 'dbb5tcahb9q41o'
     }
 });
 
-const query = (text, param) => {
-    return pool.query(text, param);
-}
-
-module.exports = {
-    query
-};
+module.exports = knex;
