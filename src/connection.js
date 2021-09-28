753c23a37c4ff0eddd90bf6c8bca3bbe88f479db
icaroaf <caroassisfreitas@globo.com>
2021-09-28 20:17:13 -0300

    atualiza configuração da conexao do db

--- src/connection.js
+++ src/connection.js
@@ -5,7 +5,10 @@ const knex = require('knex')({
         user: 'eeomqmvwtjlnrs',
         password: '9e127672ea54c7bb8a4beeff7fd437bfeafcaa8eb418c3fb243e58fd58e92cf3',
         database: 'dbb5tcahb9q41o',
-        port: 5432
+        port: 5432,
+        ssl:{
+            rejectUnauthorized: false
+        }
     }
 });
 
