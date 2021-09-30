7db34d7539d2c566553c1eee406ec5b505abcbd0
Icaro Assis <icaroassisfreitas@gmail.com>
2021-09-30 02:39:07 -0300

    cria middleware de verificação de login

---
+++ src/filters/loginVerifier.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const hashPwd = require('../hashPwd');
+const connection = require('../connection');
+const { default: knex } = require('knex');
+
+const verifyLogin = async (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if(!authorization){
+        return res.status(404).json("Token não informado");
+    }
+
+    try {
+        const token = authorization.replace('Bearer', '').trim();
+
+        const { id } = jwt.verify(token, hashPwd);
+
+        const verificationQuery = await knex('usuarios').where('id', id);
+
+        if(verificationQuery.length === 0){
+            return res.status(404).json("O usuário não foi encontrado.");
+        }
+
+        const {senha, ...usuario} = verificationQuery[0];
+
+        req.usuario = usuario;
+
+        next();
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+}
+
+module.exports = verifyLogin; 
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const users = require('./controllers/users');
 const login = require('./controllers/login');
+const verifyLogin = ('./filters/loginVerifier.js');
 
 
 const routes = express();
@@ -11,5 +12,7 @@ routes.post('/users', users.SignUpUser);
 //faz login do usuário
 routes.post('/login', login.login);
 
+//middleware para a verificação do usuário logado
+routes.use(verifyLogin);
 
 module.exports = routes;
