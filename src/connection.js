f2d8e45d5f7c6e883142d9060e0ae4eea99b9e82
Icaro Assis <icaroassisfreitas@gmail.com>
2021-09-30 22:11:18 -0300

    restabelece conexao com o db da heroku

--- src/connection.js
+++ src/connection.js
@@ -1,26 +1,26 @@
-// const knex = require('knex')({
-//     client: 'pg',
-//     connection: {
-//         host: 'ec2-54-209-52-160.compute-1.amazonaws.com',
-//         user: 'eeomqmvwtjlnrs',
-//         password: '9e127672ea54c7bb8a4beeff7fd437bfeafcaa8eb418c3fb243e58fd58e92cf3',
-//         database: 'dbb5tcahb9q41o',
-//         port: 5432,
-//         ssl:{
-//             rejectUnauthorized: false
-//         }
-//     }
-// });
-
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host : 'localhost',
-      user : 'postgres',
-      password : 'postgres',
-      database : 'testelocal_desafio',
-      port: 5432
+        host: 'ec2-54-209-52-160.compute-1.amazonaws.com',
+        user: 'eeomqmvwtjlnrs',
+        password: '9e127672ea54c7bb8a4beeff7fd437bfeafcaa8eb418c3fb243e58fd58e92cf3',
+        database: 'dbb5tcahb9q41o',
+        port: 5432,
+        ssl:{
+            rejectUnauthorized: false
+        }
     }
-  });
+});
+
+// const knex = require('knex')({
+//     client: 'pg',
+//     connection: {
+//       host : 'localhost',
+//       user : 'postgres',
+//       password : 'postgres',
+//       database : 'testelocal_desafio',
+//       port: 5432
+//     }
+//   });
 
 module.exports = knex;
