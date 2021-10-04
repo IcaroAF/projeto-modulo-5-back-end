0337caf51bb4bdd046f3b84b62d0b75a9ae51416
Icaro Assis <icaroassisfreitas@gmail.com>
2021-10-04 14:48:26 -0300

    adiciona dotenv e modifica conexao com o db

--- src/connection.js
+++ src/connection.js
@@ -1,11 +1,11 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'ec2-54-209-52-160.compute-1.amazonaws.com',
-        user: 'eeomqmvwtjlnrs',
-        password: '9e127672ea54c7bb8a4beeff7fd437bfeafcaa8eb418c3fb243e58fd58e92cf3',
-        database: 'dbb5tcahb9q41o',
-        port: 5432,
+        host: process.env.PG_HOST,
+        user: process.env.PG_USER,
+        password: process.env.PG_PASSWORD,
+        database: process.env.PG_DATABASE,
+        port: process.env.PG_PORT,
         ssl:{
             rejectUnauthorized: false
         }
